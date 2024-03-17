0f1a9e65f3d136d87f5cee9dbb8a14772c5ed1c4
Lu <81447103+anothermimich@users.noreply.github.com>
2024-03-17 14:20:11 -0300

    Add files via upload

---
+++ keymaps/vial/config.h
@@ -0,0 +1,26 @@
+// Copyright 2024 Lu (@anothermimich)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define USE_I2C         // Use protocol I2C to connect the halves
+
+// Vial
+
+#define VIAL_KEYBOARD_UID {0x7F, 0xA1, 0x8E, 0x0F, 0x0E, 0x74, 0x18, 0x1B}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0} 
+#define VIAL_UNLOCK_COMBO_COLS {0, 1} 
+
+// Firmware size reduction.
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
+
+/* disable action features */
+
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keymaps/vial/rules.mk
@@ -0,0 +1,24 @@
+# Build Options
+
+BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+AUDIO_ENABLE = no          # Audio output
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+SPLIT_KEYBOARD = yes       # Inform that it is a split keeb
+
+# VIAL config
+
+LTO_ENABLE = yes           # For pro micro
+VIA_ENABLE = yes           # Enables VIA
+VIAL_ENABLE = yes          # Enables VIAL
+
+# Space otimization
+
+EXTRAFLAGS += -flto        # Enables Link Time Optimization
+UNICODE_ENABLE = no        # Unicode
+KEY_OVERRIDE_ENABLE = no   # Disable key override
